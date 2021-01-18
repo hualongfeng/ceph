3de0f374e3675ce0be5f98a4391b3ba160519a45
lixiaoy1 <xiaoyan.li@intel.com>
2021-01-18 09:11:36 -0500

    cmake: fix rpma problem
    Signed-off-by: Li, Xiaoyan <xiaoyan.li@intel.com>

--- cmake/modules/Buildrpma.cmake
+++ cmake/modules/Buildrpma.cmake
@@ -3,24 +3,18 @@ function(build_rpma)
   set(RPMA_SRC "${CMAKE_BINARY_DIR}/src/rpma/src")
   set(RPMA_INCLUDE "${RPMA_SRC}/include")
 
-  # Use debug RPMA libs in debug lib/rbd builds
-  if(CMAKE_BUILD_TYPE STREQUAL Debug)
-    set(RPMA_LIB_DIR "debug")
-  else()
-    set(RPMA_LIB_DIR "nondebug")
-  endif()
-  set(RPMA_LIB "${RPMA_SRC}/${RPMA_LIB_DIR}")
-
   ExternalProject_Add(rpma_ext
       GIT_REPOSITORY "https://github.com/pmem/rpma.git"
-      GIT_TAG "0.9.0"
+      GIT_TAG "master"
       GIT_SHALLOW TRUE
+      SOURCE_DIR "${CMAKE_BINARY_DIR}/src/rpma"
       BINARY_DIR "${CMAKE_BINARY_DIR}/src/rpma"
       CMAKE_ARGS -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_DOC=OFF
       BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target rpma
       BUILD_BYPRODUCTS "<BINARY_DIR>/src/librpma.so"
       INSTALL_COMMAND "true")
 
+  set(RPMA_LIB "${RPMA_SRC}/")
   # librpma
   add_library(rpma::rpma STATIC IMPORTED)
   add_dependencies(rpma::rpma rpma_ext)
