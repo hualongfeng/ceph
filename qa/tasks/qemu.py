158cd666511f76788ccad08681b7cb538648d2aa
Feng Hualong <hualong.feng@intel.com>
2021-07-12 15:03:28 +0800

    compute sha256 before launching image
    Signed-off-by: Feng Hualong <hualong.feng@intel.com>

--- qa/tasks/qemu.py
+++ qa/tasks/qemu.py
@@ -463,6 +463,9 @@ def run_qemu(ctx, config):
         remote.run(args='sudo udevadm control --reload')
         remote.run(args='sudo udevadm trigger /dev/kvm')
         remote.run(args='ls -l /dev/kvm')
+        remote.run(args='rbd export rbd/client.0.0 /home/ubuntu/client.0.0')
+        remote.run(args='sha256sum /home/ubuntu/client.0.0')
+        remote.run(args='rm /home/ubuntu/client.0.0')
 
         qemu_cmd = 'qemu-system-x86_64'
         if remote.os.package_type == "rpm":
