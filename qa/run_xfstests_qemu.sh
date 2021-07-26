eedc9afe054c99f0ff5f444c59ebd74b1a96a233
Feng Hualong <hualong.feng@intel.com>
2021-07-26 09:42:55 +0800

    alone all force 8G cache size
    Signed-off-by: Feng Hualong <hualong.feng@intel.com>

--- qa/run_xfstests_qemu.sh
+++ qa/run_xfstests_qemu.sh
@@ -8,7 +8,8 @@ set -x
 [ -d "${TESTDIR}" ] || mkdir "${TESTDIR}"
 
 URL_BASE="http://10.239.40.205/software"
-SCRIPT="run_xfstests-obsolete_force_jianpeng.sh"
+#SCRIPT="run_xfstests-obsolete_force_jianpeng.sh"
+SCRIPT="run_xfstests-obsolete_force.sh"
 
 cd "${TESTDIR}"
 
@@ -18,10 +19,10 @@ chmod +x "${SCRIPT}"
 # tests excluded fail in the current testing vm regardless of whether
 # rbd is used
 
-#for testid in {1..7} {9..17} {19..26} {28..49} {51..61} 63 {66..67} {69..72} 73 74 {75..79} 83 {85..105} {108..110} {112..135} \
-#    {137..170} {174..191} {193..204} {206..217} {220..227} {230..231} 233 {235..241} {243..249} \
-#    {251..262} {264..278} {281..286} {288..289}
-for testid in 74
+#for testid in 74
+for testid in {1..7} {9..17} {19..26} {28..49} {51..61} 63 {66..67} {69..72} 73 74 {75..79} 83 {85..105} {108..110} {112..135} \
+    {137..170} {174..191} {193..204} {206..217} {220..227} {230..231} 233 {235..241} {243..249} \
+    {251..262} {264..278} {281..286} {288..289}
 do
     ./"${SCRIPT}" -c 1 -f xfs -t /dev/vdb -s /dev/vdc ${testid}
 done
