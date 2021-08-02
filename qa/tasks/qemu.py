4dc6886e2c3bf443a07cac6a2a6430353ac79b56
Feng Hualong <hualong.feng@intel.com>
2021-08-02 14:49:16 +0800

    fix bridge
    Signed-off-by: Feng Hualong <hualong.feng@intel.com>

--- qa/tasks/qemu.py
+++ qa/tasks/qemu.py
@@ -466,8 +466,9 @@ def run_qemu(ctx, config):
         remote.run(args='rbd export rbd/client.0.0 /home/ubuntu/client.0.0')
         remote.run(args='sha256sum /home/ubuntu/client.0.0')
         remote.run(args='rm /home/ubuntu/client.0.0')
+        remote.run(args='sudo apt-get -y install bridge-utils || true');
         remote.run(args='sudo ip tuntap add tap0 mode tap || true');
-        remote.run(args='sudo brctl addif br-ipsec tap0 || true');
+        remote.run(args='sudo brctl addif virbr0 tap0 || true');
 
 
         qemu_cmd = 'qemu-system-x86_64'
