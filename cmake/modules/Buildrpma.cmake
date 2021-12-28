81e462576f0aabe63411991ca2eb775780a583dc
Feng Hualong <hualong.feng@intel.com>
2021-12-28 16:35:12 +0800

    make-dist: add rpma to dist tarball
    There are two things:
      1. make-dist: add rpma to dist tarball
      2. cmake: use src/rpma for building rpma if it exists
    
    Signed-off-by: Feng Hualong <hualong.feng@intel.com>

--- cmake/modules/Buildrpma.cmake
+++ cmake/modules/Buildrpma.cmake
@@ -1,22 +1,35 @@
 function(build_rpma)
-  include(ExternalProject)
-  set(RPMA_SRC "${CMAKE_BINARY_DIR}/src/rpma/src")
-  set(RPMA_INCLUDE "${RPMA_SRC}/include")
+  include(FindMake)
 
+  if(EXISTS "${PROJECT_SOURCE_DIR}/src/rpma/CMakeLists.txt")
+    set(source_dir_args
+       SOURCE_DIR "${PROJECT_SOURCE_DIR}/src/rpma"
+       )
+  else()
+    set(source_dir_args
+	SOURCE_DIR ${CMAKE_BINARY_DIR}/src/rpma
+	GIT_REPOSITORY https://github.com/pmem/rpma.git
+	GIT_TAG "0.9.0"
+	GIT_SHALLOW TRUE
+	GIT_CONFIG advice.detachedHead=false
+	)
+  endif()
+
+  include(ExternalProject)
   ExternalProject_Add(rpma_ext
-      GIT_REPOSITORY "https://github.com/pmem/rpma.git"
-      GIT_TAG "master"
-      GIT_SHALLOW TRUE
-      SOURCE_DIR "${CMAKE_BINARY_DIR}/src/rpma"
-      BINARY_DIR "${CMAKE_BINARY_DIR}/src/rpma"
+      ${source_dir_args}
       CMAKE_ARGS -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_DOC=OFF
-      BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target rpma
-      BUILD_BYPRODUCTS "<BINARY_DIR>/src/librpma.so"
+      BUILD_COMMAND ${CMAKE_COMMAND} --build <SOURCE_DIR> --target rpma
+      BUILD_BYPRODUCTS "<SOURCE_DIR>/src/librpma.so"
       INSTALL_COMMAND "true")
 
+  ExternalProject_Get_Property(rpma_ext source_dir)
+  set(RPMA_SRC "${source_dir}/src")
+  set(RPMA_INCLUDE "${source_dir}/src/include")
   set(RPMA_LIB "${RPMA_SRC}/")
+
   # librpma
-  add_library(rpma::rpma STATIC IMPORTED)
+  add_library(rpma::rpma STATIC IMPORTED GLOBAL)
   add_dependencies(rpma::rpma rpma_ext)
   file(MAKE_DIRECTORY ${RPMA_INCLUDE})
   set_target_properties(rpma::rpma PROPERTIES
