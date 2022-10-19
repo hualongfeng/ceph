9df07a13e9f1db866897da256a9f6fae88ae3d99
Feng, Hualong <hualong.feng@intel.com>
2022-10-19 02:59:50 +0000

    bash hash qat mode
    Signed-off-by: Feng, Hualong <hualong.feng@intel.com>

--- src/common/ceph_crypto.cc
+++ src/common/ceph_crypto.cc
@@ -17,6 +17,7 @@
 #include "common/ceph_context.h"
 #include "common/ceph_mutex.h"
 #include "common/config.h"
+#include "common/Thread.h"
 #include "ceph_crypto.h"
 
 #include <openssl/evp.h>
@@ -32,6 +33,60 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+#include <iostream>
+#include <semaphore.h>
+#include <string.h>
+#include <vector>
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+extern "C" {
+#include "cpa.h"
+#include "lac/cpa_cy_sym.h"
+#include "lac/cpa_cy_im.h"
+#include "qae_mem.h"
+#include "icp_sal_user.h"
+#include "icp_sal_poll.h"
+#include "qae_mem_utils.h"
+}
+
+#define BUFFER_SIZE 131072
+
+struct completion_struct
+{
+  sem_t semaphore;
+};
+/* Use semaphores to signal completion of events */
+#define COMPLETION_STRUCT completion_struct
+
+#define COMPLETION_INIT(s) sem_init(&((s)->semaphore), 0, 0);
+
+#define COMPLETION_WAIT(s, timeout) (sem_wait(&((s)->semaphore)) == 0)
+
+#define COMPLETE(s) sem_post(&((s)->semaphore))
+
+#define COMPLETION_DESTROY(s) sem_destroy(&((s)->semaphore))
+
+static __inline CpaStatus msSleep(Cpa32U ms)
+{
+  int ret = 0;
+  struct timespec resTime, remTime;
+  resTime.tv_sec = ms / 1000;
+  resTime.tv_nsec = (ms % 1000) * 1000000;
+  do {
+    ret = nanosleep(&resTime, &remTime);
+    resTime = remTime;
+  } while ((ret != 0) && (errno == EINTR));
+
+  if (ret != 0) {
+    std::cout << "nanoSleep failed with code "<< ret << std::endl;
+    return CPA_STATUS_FAIL;
+  } else {
+    return CPA_STATUS_SUCCESS;
+  }
+}
 
 namespace TOPNSPC::crypto::ssl {
 
@@ -218,6 +273,502 @@ void ssl::OpenSSLDigest::Final(unsigned char *digest) {
   EVP_DigestFinal_ex(mpContext, digest, &s);
 }
 
+}
+
+namespace TOPNSPC::crypto::qat {
+class QatInstancesManager {
+  std::vector<CpaInstanceHandle> cyInstances;
+  std::mutex lock;
+  std::atomic<size_t> index{0};
+  std::thread poll_thread;
+  volatile bool gPollingCy{false};
+  void init();
+  bool inited{false};
+ public:
+  void poll_instance();
+  QatInstancesManager();
+  ~QatInstancesManager();
+  CpaInstanceHandle getInstance();
+};
+
+QatInstancesManager::QatInstancesManager() : index(0) {}
+
+
+void QatInstancesManager::init() {
+  CpaStatus stat = CPA_STATUS_SUCCESS;
+  Cpa16U numInstances = 0;
+//  std::cout << "QatInstancesManager constructor" << std::endl;
+
+  stat = qaeMemInit();
+  if (CPA_STATUS_SUCCESS != stat)
+  {
+    std::cout << "Failed to initialize memory driver" << std::endl;
+    throw "Failed to initialize memory driver";
+  }
+
+  stat = icp_sal_userStartMultiProcess("SHIM", CPA_FALSE);
+  if (CPA_STATUS_SUCCESS != stat)
+  {
+    std::cout << "Failed to start user process SHIM" << std::endl;
+    qaeMemDestroy();
+    throw "Failed to start user process SHIM";
+  }
+
+  stat = cpaCyGetNumInstances(&numInstances);
+  if ((stat == CPA_STATUS_SUCCESS) && (numInstances > 0)) {
+    cyInstances.resize(numInstances);
+    stat = cpaCyGetInstances(numInstances, &cyInstances[0]);
+  }
+
+  if ((stat != CPA_STATUS_SUCCESS) || (numInstances == 0)) {
+    icp_sal_userStop();
+    qaeMemDestroy();
+
+    std::cout << "No instances found for 'SHIM'" << std::endl;
+    throw "No instances found for 'SHIM'";
+  }
+
+  //std::cout << "cpaCyStartInstance: " << numInstances << std::endl;
+  for (auto &instance : cyInstances) {
+    cpaCyStartInstance(instance);
+    cpaCySetAddressTranslation(instance, qaeVirtToPhysNUMA);
+    CpaInstanceInfo2 info2;
+    stat = cpaCyInstanceGetInfo2(instance, &info2);
+    if ((stat == CPA_STATUS_SUCCESS) && (info2.isPolled == CPA_TRUE)) {
+      /*do nothing*/
+    } else {
+      icp_sal_userStop();
+      qaeMemDestroy();
+
+      std::cout << "One instance cannot poll, check the config" << std::endl;
+      throw "One instance cannot poll, check the config";
+    }
+  }
+  //std::cout << "start poll instance" << std::endl;
+  //poll_thread = std::thread(&QatInstancesManager::poll_instance, this);
+  poll_thread = make_named_thread("qat_hash_poll", &QatInstancesManager::poll_instance, this);
+}
+
+QatInstancesManager::~QatInstancesManager() {
+//  std::cout << "QatInstancesManager destructor" << std::endl;
+  gPollingCy = false;
+  if (poll_thread.joinable()) {
+    poll_thread.join();
+  }
+  for (auto &instance : cyInstances) {
+    cpaCyStopInstance(instance);
+  }
+  icp_sal_userStop();
+  qaeMemDestroy();
+}
+
+CpaInstanceHandle QatInstancesManager::getInstance() {
+  std::lock_guard<std::mutex> l{lock};
+  if (!inited) {
+    init();
+    inited = true;
+    //poll_thread = std::thread(&QatInstancesManager::poll_instance, this);
+  }
+  CpaInstanceHandle instance = cyInstances[index++];
+  if (index >= cyInstances.size()) index = 0;
+  return instance;
+}
+
+void QatInstancesManager::poll_instance() {
+  //std::cout << "start poll instance" << std::endl;
+  gPollingCy = true;
+  while (gPollingCy) {
+    Cpa64U requestsCount = 0;
+    int cnt = 0;
+    for (auto &instance : cyInstances) {
+      CpaCySymStats64 stat{0};
+      cpaCySymQueryStats64(instance, &stat);
+    //  std::cout << "numSymOpRequests = " << stat.numSymOpRequests
+    //            << ", numSymOpCompleted = " << stat.numSymOpCompleted
+    //            << ", numSessionsInitialized = " << stat.numSessionsInitialized
+    //            << ", numSessionsRemoved = " << stat.numSessionsRemoved
+    //	          << ", num = " << cnt++
+    //            << std::endl;
+
+      requestsCount += (stat.numSymOpRequests - stat.numSymOpCompleted);
+      if (stat.numSymOpRequests != stat.numSymOpCompleted) {
+        icp_sal_CyPollInstance(instance, 0);
+      }
+    }
+    if (requestsCount == 0) {
+      msSleep(1);
+    }
+  }
+}
+
+static inline void qcc_contig_mem_free(void **ptr) {
+  if (*ptr) {
+    qaeMemFreeNUMA(ptr);
+    *ptr = nullptr;
+  }
+}
+
+static inline CpaStatus qcc_contig_mem_alloc(void **ptr, Cpa32U size, Cpa32U alignment = 1) {
+  *ptr = qaeMemAllocNUMA(size, 0, alignment);
+  if (nullptr == *ptr) {
+    return CPA_STATUS_RESOURCE;
+  }
+  return CPA_STATUS_SUCCESS;
+}
+
+static void symCallback(void *pCallbackTag,
+                        CpaStatus status,
+                        const CpaCySymOp operationType,
+                        void *pOpData,
+                        CpaBufferList *pDstBuffer,
+                        CpaBoolean verifyResult)
+{
+  if (nullptr != pCallbackTag)
+  {
+    /** indicate that the function has been called*/
+    COMPLETE((struct COMPLETION_STRUCT *)pCallbackTag);
+  }
+}
+
+
+static Cpa32U digest_size(CpaCySymHashAlgorithm _type) {
+  switch(_type) {
+    case CPA_CY_SYM_HASH_MD5:
+      return CEPH_CRYPTO_MD5_DIGESTSIZE;
+    case CPA_CY_SYM_HASH_SHA1:
+      return CEPH_CRYPTO_SHA1_DIGESTSIZE;
+    case CPA_CY_SYM_HASH_SHA256:
+      return CEPH_CRYPTO_SHA256_DIGESTSIZE;
+    case CPA_CY_SYM_HASH_SHA512:
+      return CEPH_CRYPTO_SHA512_DIGESTSIZE;
+  }
+  return -1;
+}
+
+static Cpa32U block_size(CpaCySymHashAlgorithm _type) {
+  switch(_type) {
+    case CPA_CY_SYM_HASH_MD5:
+      return LAC_HASH_MD5_BLOCK_SIZE;
+    case CPA_CY_SYM_HASH_SHA1:
+      return LAC_HASH_SHA1_BLOCK_SIZE;
+    case CPA_CY_SYM_HASH_SHA256:
+      return LAC_HASH_SHA256_BLOCK_SIZE;
+    case CPA_CY_SYM_HASH_SHA512:
+      return LAC_HASH_SHA512_BLOCK_SIZE;
+  }
+  return -1;
+}
+
+static QatInstancesManager qat_instance_manager;
+
+QatHashCommon::QatHashCommon(const CpaCySymHashAlgorithm _type)
+	: cyInstHandle(qat_instance_manager.getInstance()), mpType(_type) {
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  digest_length = digest_size(mpType);
+  block_length = block_size(mpType);
+  align_left = new unsigned char[LAC_HASH_MAX_BLOCK_SIZE];
+  sessionSetupData = new CpaCySymSessionSetupData;
+  memset(sessionSetupData, 0, sizeof(CpaCySymSessionSetupData));
+  sessionSetupData->sessionPriority = CPA_CY_PRIORITY_NORMAL;
+  sessionSetupData->symOperation = CPA_CY_SYM_OP_HASH;
+  sessionSetupData->hashSetupData.hashAlgorithm = mpType;
+  sessionSetupData->hashSetupData.hashMode = CPA_CY_SYM_HASH_MODE_PLAIN;
+  sessionSetupData->hashSetupData.digestResultLenInBytes = digest_length;
+  sessionSetupData->digestIsAppended = CPA_FALSE;
+  sessionSetupData->verifyDigest = CPA_FALSE;
+}
+
+
+  QatHashCommon::QatHashCommon(QatHashCommon &&qat) {
+    cyInstHandle     = qat.cyInstHandle;
+    mpType           = qat.mpType;
+    sessionCtx       = qat.sessionCtx;
+    pBufferList      = qat.pBufferList;
+    digest_length    = qat.digest_length;
+    block_length     = qat.block_length;
+    align_left       = qat.align_left;
+    align_left_len   = qat.align_left_len;
+    sessionSetupData = qat.sessionSetupData;
+    partial          = qat.partial;
+
+    qat.cyInstHandle = nullptr;
+    qat.mpType = CPA_CY_SYM_HASH_NONE;
+    qat.sessionCtx = nullptr;
+    qat.pBufferList = nullptr;
+    qat.digest_length = 0;
+    qat.block_length = 0;
+    qat.align_left = nullptr;
+    qat.align_left_len = 0;
+    qat.sessionSetupData = nullptr;
+    qat.partial = false;
+  }
+
+QatHashCommon::~QatHashCommon() {
+  void *pBufferMeta = nullptr;
+  void *pSrcBuffer = nullptr;
+  void *pDigestBuffer = nullptr;
+
+  if (pBufferList != nullptr) {
+    pBufferMeta   = pBufferList->pPrivateMetaData;
+    pDigestBuffer = pBufferList->pUserData;
+    pSrcBuffer    = pBufferList->pBuffers->pData;
+    qcc_contig_mem_free(&pSrcBuffer);
+    free(pBufferList);
+    qcc_contig_mem_free(&pBufferMeta);
+    qcc_contig_mem_free((void**)&pDigestBuffer);
+  }
+
+  if (sessionCtx != nullptr) {
+    CpaBoolean sessionInUse = CPA_FALSE;
+    do {
+      cpaCySymSessionInUse(sessionCtx, &sessionInUse);
+    } while (sessionInUse);
+
+    cpaCySymRemoveSession(cyInstHandle, sessionCtx);
+    qcc_contig_mem_free((void**)&sessionCtx);
+  }
+
+  delete[] align_left;
+  delete sessionSetupData;
+}
+
+void QatHashCommon::Restart() {
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  Cpa32U sessionCtxSize;
+  if (sessionCtx == nullptr) {
+    // first need to alloc session memory
+    status = cpaCySymSessionCtxGetSize(cyInstHandle, sessionSetupData, &sessionCtxSize);
+    if (status != CPA_STATUS_SUCCESS) {
+      std::cout <<  "cpaCySymSessionCtxGetSize failed, stat = " << status << std::endl;
+      throw "cpaCySymSessionCtxGetSize failed";
+    }
+    status = qcc_contig_mem_alloc((void**)&sessionCtx, sessionCtxSize);
+    if (status != CPA_STATUS_SUCCESS) {
+      std::cout << "Failed to alloc contiguous memory for SessionCtx, stat = " << status << std::endl;
+      throw "Failed to alloc contiguous memory for SessionCtx";
+    }
+  }
+
+  status = cpaCySymInitSession(cyInstHandle, symCallback, sessionSetupData, sessionCtx);
+  if (status != CPA_STATUS_SUCCESS) {
+    std::cout << "cpaCySymInitSession failed, stat = " << status << std::endl;;
+    throw "cpaCySymInitSession failed";
+  }
+}
+
+/*******************************************************************************************
+ **   -----------------|---------------------------|
+ **                    |-------numBuffers----------|
+ **    CpaBufferList   |-------pBuffers------------|-----
+ **                    |-------pUserData-----------|    |
+ **                    |-------pPrivateMetaData----|    |
+ **   -----------------|---------------------------|<----
+ **                    |-------dataLenInByte-------|      -----------------------------
+ **    CpaFlatBuffer   |-------pData---------------|---->|contiguous memory| pSrcBuffer
+ **   -----------------|---------------------------|      -----------------------------
+*********************************************************************************************/
+
+CpaBufferList* QatHashCommon::getCpaBufferList() {
+
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  Cpa8U *pBufferMeta = nullptr;
+  Cpa32U bufferMetaSize = 0;
+  CpaBufferList *pBufferList = nullptr;
+  CpaFlatBuffer *pFlatBuffer = nullptr;
+  Cpa32U bufferSize = BUFFER_SIZE;
+  Cpa32U numBuffers = 1;
+  Cpa32U bufferListMemSize = sizeof(CpaBufferList) + (numBuffers * sizeof(CpaFlatBuffer));
+  Cpa8U *pSrcBuffer = nullptr;
+  Cpa8U *pDigestBuffer = nullptr;
+
+  status = cpaCyBufferListGetMetaSize(cyInstHandle, numBuffers, &bufferMetaSize);
+
+  if (CPA_STATUS_SUCCESS == status)
+  {
+    status = qcc_contig_mem_alloc((void**)&pBufferMeta, bufferMetaSize);
+  }
+
+  if (CPA_STATUS_SUCCESS == status)
+  {
+    pBufferList = (CpaBufferList *)malloc(bufferListMemSize);
+    if (pBufferList == nullptr) return nullptr;
+  }
+
+  if (CPA_STATUS_SUCCESS == status)
+  {
+    status = qcc_contig_mem_alloc((void**)&pSrcBuffer, bufferSize);
+  }
+
+  if (CPA_STATUS_SUCCESS == status)
+  {
+    status = qcc_contig_mem_alloc((void**)&pDigestBuffer, digest_length);
+  }
+
+  if (CPA_STATUS_SUCCESS == status)
+  {
+    /* increment by sizeof(CpaBufferList) to get at the
+     * array of flatbuffers */
+    pFlatBuffer = (CpaFlatBuffer *)(pBufferList + 1);
+
+    pBufferList->numBuffers = 1;
+    pBufferList->pBuffers = pFlatBuffer;
+    pBufferList->pUserData = pDigestBuffer;
+    pBufferList->pPrivateMetaData = pBufferMeta;
+
+    pFlatBuffer->dataLenInBytes = bufferSize;
+    pFlatBuffer->pData = pSrcBuffer;
+
+  }
+
+  return pBufferList;
+}
+
+void QatHashCommon::Update(const unsigned char *input, size_t length) {
+//  std::cout << "Update input length: " << length
+//            << "align_left_len: " << align_left_len << std::endl;
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  CpaCySymOpData pOpData = {0};
+
+  size_t left_length = length;
+  size_t current_length = 0;
+  size_t new_write_length = 0;
+  size_t current_align_left_len = 0;
+
+  if (pBufferList == nullptr) {
+    pBufferList = getCpaBufferList();
+
+    if (pBufferList == nullptr) {
+      std::cout << "cannot get CpaBufferList" << std::endl;
+      return;
+    }
+  }
+
+  struct COMPLETION_STRUCT complete;
+
+  COMPLETION_INIT((&complete));
+
+  do {
+    new_write_length = left_length > (BUFFER_SIZE - align_left_len) ? \
+	    (BUFFER_SIZE - align_left_len) : left_length;
+    current_length = new_write_length + align_left_len;
+    left_length -= new_write_length;
+
+    if (current_length % block_length == 0) {
+      memcpy(pBufferList->pBuffers->pData, align_left, align_left_len);
+      memcpy(pBufferList->pBuffers->pData + align_left_len, input, new_write_length);
+      align_left_len = 0;
+    } else if (current_length / block_length > 0){
+      memcpy(pBufferList->pBuffers->pData, align_left, align_left_len);
+      current_align_left_len = current_length % block_length;
+      memcpy(pBufferList->pBuffers->pData + align_left_len, input, new_write_length - current_align_left_len);
+      memcpy(align_left, input + new_write_length - current_align_left_len, current_align_left_len);
+      align_left_len = current_align_left_len;
+      current_length -= align_left_len;
+    } else {
+      memcpy(align_left + align_left_len, input, new_write_length);
+      align_left_len += new_write_length;
+      current_length = 0;
+//      std::cout << "Update " << "align_left_len: " << align_left_len << std::endl;
+      break ;
+    }
+
+    input += new_write_length;
+
+//    std::cout << "Update current_length: " << current_length
+//	      << ", align_left_len: " << align_left_len
+//	      << ", new_write_length" << new_write_length
+//	      << std::endl;
+
+    pBufferList->pBuffers->dataLenInBytes = current_length;
+    pOpData.packetType = CPA_CY_SYM_PACKET_TYPE_PARTIAL;
+    pOpData.sessionCtx = sessionCtx;
+    pOpData.hashStartSrcOffsetInBytes = 0;
+    pOpData.messageLenToHashInBytes = current_length;
+    pOpData.pDigestResult = (Cpa8U *)pBufferList->pUserData;
+    do {
+      status = cpaCySymPerformOp(
+        cyInstHandle,
+        (void *)&complete,
+        &pOpData,
+        pBufferList,
+        pBufferList,
+        NULL);
+    } while (status == CPA_STATUS_RETRY);
+    partial = true;
+    if (CPA_STATUS_SUCCESS != status) {
+      std::cout << "Update cpaCySymPerformOp failed. (status = " << status << std::endl;
+    }
+
+    if (CPA_STATUS_SUCCESS == status) {
+      if (!COMPLETION_WAIT((&complete), TIMEOUT_MS)) {
+        std::cout << "timeout or interruption in cpaCySymPerformOp" << std::endl;
+      }
+    }
+  } while (left_length > 0);
+  COMPLETION_DESTROY(&complete);
+}
+
+void QatHashCommon::Final(unsigned char *digest) {
+//  std::cout << "Final start" << std::endl;
+  CpaStatus status = CPA_STATUS_SUCCESS;
+  if (pBufferList == nullptr) {
+    pBufferList = getCpaBufferList();
+
+    if (pBufferList == nullptr) {
+      std::cout << "cannot get CpaBufferList" << std::endl;
+      return;
+    }
+  }
+
+  CpaCySymOpData pOpData = {0};
+
+  struct COMPLETION_STRUCT complete;
+
+  COMPLETION_INIT((&complete));
+
+  pBufferList->pBuffers->dataLenInBytes = 0;
+  if (align_left_len > 0) {
+
+//    std::cout << "align_left_len: " << align_left_len << std::endl;
+    memcpy(pBufferList->pBuffers->pData, align_left, align_left_len);
+    pBufferList->pBuffers->dataLenInBytes = align_left_len;
+    align_left_len = 0;
+  }
+
+  if (partial) {
+    pOpData.packetType = CPA_CY_SYM_PACKET_TYPE_LAST_PARTIAL;
+  } else {
+    pOpData.packetType = CPA_CY_SYM_PACKET_TYPE_FULL;
+  }
+  pOpData.sessionCtx = sessionCtx;
+  pOpData.hashStartSrcOffsetInBytes = 0;
+  pOpData.messageLenToHashInBytes = pBufferList->pBuffers->dataLenInBytes;
+  pOpData.pDigestResult = (Cpa8U *)pBufferList->pUserData;
+
+  do {
+    status = cpaCySymPerformOp(
+      cyInstHandle,
+      (void *)&complete,
+      &pOpData,
+      pBufferList,
+      pBufferList,
+      NULL);
+
+  } while (status == CPA_STATUS_RETRY);
+  if (CPA_STATUS_SUCCESS != status) {
+    std::cout << "Final cpaCySymPerformOp failed. (status = " << status << std::endl;
+  }
+
+  if (CPA_STATUS_SUCCESS == status) {
+    if (!COMPLETION_WAIT((&complete), TIMEOUT_MS)) {
+      std::cout << "timeout or interruption in cpaCySymPerformOp" << std::endl;
+    }
+  }
+  COMPLETION_DESTROY(&complete);
+
+  memcpy(digest, pOpData.pDigestResult, digest_length);
+//  std::cout << "Final end" << std::endl;
+}
 }
 
 #pragma clang diagnostic pop
--- src/common/ceph_crypto.h
+++ src/common/ceph_crypto.h
@@ -9,6 +9,19 @@
 #include "include/buffer.h"
 #include "include/types.h"
 
+extern "C" {
+#include "cpa.h"
+#include "lac/cpa_cy_sym.h"
+}
+
+//quickassist/lookaside/access_layer/src/common/crypto/sym/include/lac_sym_hash_defs.h
+#define LAC_HASH_MD5_BLOCK_SIZE    64
+#define LAC_HASH_SHA1_BLOCK_SIZE   64
+#define LAC_HASH_SHA256_BLOCK_SIZE 64
+#define LAC_HASH_SHA512_BLOCK_SIZE 128
+#define LAC_HASH_MAX_BLOCK_SIZE LAC_HASH_SHA512_BLOCK_SIZE
+
+
 #define CEPH_CRYPTO_MD5_DIGESTSIZE 16
 #define CEPH_CRYPTO_HMACSHA1_DIGESTSIZE 20
 #define CEPH_CRYPTO_SHA1_DIGESTSIZE 20
@@ -187,13 +200,118 @@ namespace TOPNSPC::crypto {
 }
 
 
-  using ssl::SHA256;
-  using ssl::MD5;
-  using ssl::SHA1;
-  using ssl::SHA512;
+  namespace qat {
+  class QatHashCommon {
+   private:
+    CpaInstanceHandle cyInstHandle{nullptr};
+    CpaCySymSessionCtx sessionCtx{nullptr};
+    CpaBufferList *pBufferList{nullptr};
+    size_t digest_length{0};
+    CpaBufferList* getCpaBufferList();
+    // partial packet need to align block length
+    size_t block_length{0};
+    unsigned char* align_left;
+    size_t align_left_len{0};
+    bool partial{false};
+   protected:
+    CpaCySymSessionSetupData* sessionSetupData;
+    CpaCySymHashAlgorithm mpType{CPA_CY_SYM_HASH_NONE};
+   public:
+    QatHashCommon (const CpaCySymHashAlgorithm mpType);
+    QatHashCommon (const QatHashCommon &qat) = delete;
+    QatHashCommon (QatHashCommon &&qat);
+    ~QatHashCommon ();
+    void Restart();
+    void SetFlags(int flags){}
+    void Update(const unsigned char *input, size_t length);
+    void Final(unsigned char *digest);
+  };
 
-  using ssl::HMACSHA256;
-  using ssl::HMACSHA1;
+
+  class QatDigest : public QatHashCommon {
+   public:
+    QatDigest (const CpaCySymHashAlgorithm mpType) :
+	  QatHashCommon(mpType) {
+      this->Restart();
+    }
+    QatDigest (const QatDigest& qat_digest) = delete;
+
+    QatDigest (QatDigest&& qat_digest) : QatHashCommon(std::move(qat_digest)){};
+  };
+
+  class MD5 : public QatDigest {
+   public:
+    static constexpr size_t digest_size = CEPH_CRYPTO_MD5_DIGESTSIZE;
+    MD5() : QatDigest(CPA_CY_SYM_HASH_MD5) {}
+  };
+
+  class SHA1 : public QatDigest {
+   public:
+    static constexpr size_t digest_size = CEPH_CRYPTO_SHA1_DIGESTSIZE;
+    SHA1() : QatDigest(CPA_CY_SYM_HASH_SHA1) {}
+  };
+
+  class SHA256 : public QatDigest {
+   public:
+    static constexpr size_t digest_size = CEPH_CRYPTO_SHA256_DIGESTSIZE;
+    SHA256() : QatDigest(CPA_CY_SYM_HASH_SHA256) {}
+  };
+
+  class SHA512 : public QatDigest {
+   public:
+    static constexpr size_t digest_size = CEPH_CRYPTO_SHA512_DIGESTSIZE;
+    SHA512() : QatDigest(CPA_CY_SYM_HASH_SHA512) {}
+  };
+
+  class QatHMAC : public QatHashCommon {
+   public:
+    QatHMAC (const CpaCySymHashAlgorithm mpType,
+	   const unsigned char *key, size_t length) :
+	  QatHashCommon(mpType) {
+      sessionSetupData->hashSetupData.hashMode = CPA_CY_SYM_HASH_MODE_AUTH;
+      sessionSetupData->hashSetupData.authModeSetupData.authKey = const_cast<Cpa8U*>(key);
+      sessionSetupData->hashSetupData.authModeSetupData.authKeyLenInBytes = length;
+      this->Restart();
+    }
+    QatHMAC (const QatHMAC& qat_hmac) = delete;
+    QatHMAC (QatHMAC&& qat_hmac) :
+	  QatHashCommon(std::move(qat_hmac)) {
+    }
+
+  };
+
+  class HMACSHA1 : public QatHMAC {
+   public:
+    static constexpr size_t digest_size = CEPH_CRYPTO_HMACSHA1_DIGESTSIZE;
+    HMACSHA1(const unsigned char *key, size_t length) :
+	  QatHMAC(CPA_CY_SYM_HASH_SHA1, key, length){}
+  };
+
+  class HMACSHA256 : public QatHMAC {
+   public:
+    static constexpr size_t digest_size = CEPH_CRYPTO_HMACSHA256_DIGESTSIZE;
+    HMACSHA256(const unsigned char *key, size_t length) :
+	  QatHMAC(CPA_CY_SYM_HASH_SHA256, key, length){}
+  };
+}
+
+//  using ssl::SHA256;
+//  using ssl::MD5;
+//  using ssl::SHA1;
+//  using ssl::SHA512;
+
+//  using ssl::HMACSHA256;
+//  using ssl::HMACSHA1;
+
+  using qat::SHA256;
+  using qat::MD5;
+  using qat::SHA1;
+  using qat::SHA512;
+
+  using qat::HMACSHA256;
+  using qat::HMACSHA1;
+
+
 
 template<class Digest>
 auto digest(const ceph::buffer::list& bl)
